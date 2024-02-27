ae9fc6095179abe1657c4589950fbbea731ed6e9
rashadalh <alhaddad.rashad@gmail.com>
2024-02-27 13:41:11 -0700

    adds HIRO inscrioptions API + fixes jest test cases

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 To install  
 ```
 1. Update submodules -- git submodule --update init (this will sync forked repos)
-2. Run npm install to install node modules for typescrip
+2. Run npm install to install node modules for typescript
 3. Foundry -- If not installed run curl -L https://foundry.paradigm.xyz | bash, follow instructions
-4. Change into solidity directory
+4. TODO -- SOLIDITY INSTRUCTION FOR LOCAL TESTING WITH HARDHAT
 ```
 
 To run
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,106 @@
 /**
  * Test cases with jest
  */
+// Import your fetchInscriptions function
+import { fetchInscriptionForAddr } from '../apis/hiroInsByAddr';
+import { addrHasInscription } from "../apis/hiroInsByAddr";
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.enableMocks();
+
+
+// Enable fetch mocks
+beforeEach(() => {
+  fetchMock.resetMocks();
+});
+
+describe('fetchInscriptions', () => {
+  it('fetches inscriptions successfully from an API', async () => {
+    const mockResponse = {
+      limit: 20,
+      offset: 0,
+      total: 1,
+      results: [
+        {
+          id: "1e3ba4db931e67015e3d50f336b06584ea5b58067137aeadbe20db8e12b5e226i0",
+          // Add the rest of the properties as needed for your test
+        },
+      ],
+    };
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+
+    const inscriptionId = '1e3ba4db931e67015e3d50f336b06584ea5b58067137aeadbe20db8e12b5e226i0';
+    const address = 'bc1qu7adnr8pxpaelm83xlhf0979dzxzjs2qucjh49';
+    
+    const response = await fetchInscriptionForAddr(inscriptionId, address);
+
+    // Check if the fetch was called with the correct URL
+    expect(fetch).toHaveBeenCalledWith(`https://api.hiro.so/ordinals/v1/inscriptions?id=${inscriptionId}&address=${address}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    // Verify the structure of the response
+    expect(response).toEqual(mockResponse);
+  });
+  it('Should return true that our mock response has the inscription', async () => {
+    const mockResponse = {
+      limit: 20,
+      offset: 0,
+      total: 1,
+      results: [
+        {
+          id: "1e3ba4db931e67015e3d50f336b06584ea5b58067137aeadbe20db8e12b5e226i0",
+          // Add the rest of the properties as needed for your test
+        },
+      ],
+    };
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+
+    const inscriptionId = '1e3ba4db931e67015e3d50f336b06584ea5b58067137aeadbe20db8e12b5e226i0';
+    const address = 'bc1qu7adnr8pxpaelm83xlhf0979dzxzjs2qucjh49';
+    
+    const response = await addrHasInscription(address, inscriptionId);
+
+    // Check if the fetch was called with the correct URL
+    expect(fetch).toHaveBeenCalledWith(`https://api.hiro.so/ordinals/v1/inscriptions?id=${inscriptionId}&address=${address}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    // Verify the structure of the response
+    expect(response).toEqual(true);
+  });
+  it('Should return false that our mock response does not have the inscription', async () => {
+    const mockResponse = {
+      limit: 20,
+      offset: 0,
+      total: 0,
+      results: [],
+    };
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+
+    const inscriptionId = '1e3ba4db931e67015e3d50f336b06584ea5b58067137aeadbe20db8e12b5e226i0';
+    const address = 'bc1qu7adnr8pxpaelm83xlhf0979dzxzjs2qucjh49';
+    
+    const response = await addrHasInscription(address, inscriptionId);
+
+    // Check if the fetch was called with the correct URL
+    expect(fetch).toHaveBeenCalledWith(`https://api.hiro.so/ordinals/v1/inscriptions?id=${inscriptionId}&address=${address}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    // Verify the structure of the response
+    expect(response).toEqual(false);
+  });
+});
